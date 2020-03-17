bff00c8ac0d6b9d798d11f0f9d97c7e3ea2f8946
Jelmer Lubberts <jelmer_lubberts@hotmail.com>
2020-03-17 13:22:54 +0100

    clean routes.js and server.js and make it work again

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,15 @@
 const express   = require('express');
 const routes    = express.Router();
+const fs          = require('fs');
+const bodyParser  = require('body-parser')
 const data      = require('../views/data.json') //local data in JSON
 const User      = require('../models/user')
 const Activity  = require('../models/activity')
 
+// configure body-parser to use Post requests
+routes
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json());
 
 //routes
 routes //check client request for fun
@@ -20,44 +26,99 @@ routes// define routes
   .get('/inlog',        (req, res) => {res.render('pages/inlog')})
   .get('/register',     (req, res) => {res.render('pages/register')})
   .get('/activiteiten', parseActivityData)
-  .get('/users',        parseUserData)
   .get('/activiteiten/:_id', parseAcitivityDetail)
+  .get('/nieuwe_activiteit/', (req, res) => {res.render('pages/nieuwe_activiteit')})
+  .get('/users',        parseUserData)
 
-async function parseAcitivityDetail (req,res) {
+routes
+  .post('/register', register)
+  .post('/activiteiten', newActivity)
+
+// GET method functions
 
 
+  async function parseAcitivityDetail (req,res) {
+      const activiteit = await Activity.find({_id: req.params})
 
-    const activiteit = await Activity.find({_id: req.params})
+      res.render('pages/activiteitendetail', { activiteit:activiteit} )
+    }
 
-    res.render('pages/activiteitendetail', { activiteit:activiteit} )
+  function parseLocalData (req, res){ //load data from data.json
+      res.render('pages', {activiteiten: data});
   }
 
+  async function parseUserData (req, res){//Load data from users into /users
+    const users = await User.find({});
 
-function parseLocalData (req, res){
-  res.render('pages', {activiteiten: data});
-}
-
-//Load data from users into /users
-async function parseUserData (req, res){
-  const users = await User.find({});
-
-  try{
-    res.render('pages/users', {users: users})
-  } catch (err) {
-    res.status(500).send(err);
+    try{
+      res.render('pages/users', {users: users})
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
-}
-//Load data from activities into /activities
-async function parseActivityData (req, res){
 
-  const activiteiten = await Activity.find({}).limit(4);
+  async function parseActivityData (req, res){//Load data from activities into /activities
+    let title = req.query.title
+    let activiteiten;
+    console.log(title)
 
-  console.log(activiteiten.length)
-  try{
-    res.render('pages/activiteiten', {activiteiten: activiteiten})
-  } catch (err) {
-    res.status(500).send(err);
+    if (!req.query.title){
+       activiteiten = await Activity.find({})//.limit(4);
+    } else{
+       activiteiten = await Activity.find({activity_title: title})
+    }
+
+    try{
+      res.render('pages/activiteiten', {activiteiten: activiteiten})
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
-}
 
+//POST-method Functions
+
+function register (req,res){
+
+    // save User with promises
+   const userFormat = {
+     name: req.body.name,
+     email: req.body.email,
+     password: req.body.password
+   }
+
+console.log(userFormat)
+
+   async function runCode () {
+     const user = new User(userFormat) //add user
+     const doc = await user.save()
+     console.log(doc)
+     // drop collection await User.deleteMany({})
+   }
+
+   runCode()
+     .catch(error => { console.log(error)})
+
+     res.redirect('/register')
+  }
+
+function newActivity(req, res){
+
+    // save Activity with promises
+   const activityFormat = {
+     activity_title: req.body.activity_title,
+     org_name: req.body.org_name,
+     date: req.body.date
+   }
+
+   async function runCode () {
+     const activity = new Activity(activityFormat) //add user
+     const doc = await activity.save()
+     // drop collection await Activity.deleteMany({})
+   }
+
+   runCode()
+     .catch(error => { console.log(error)})
+
+     res.redirect('/activiteiten')
+}
 module.exports = routes;
--- server.js
+++ server.js
@@ -18,56 +18,6 @@ app
   .use('/assets', express.static('assets')) // assets folder
   .use('/', routes) //routes
 
-// configure body-parser to use Post requests
-app
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(bodyParser.json());
-
- app.
-  post('/register',  (req,res) => {
-
-    // save User with promises
-   const userFormat = {
-     name: req.body.name,
-     email: req.body.email,
-     password: req.body.password
-   }
-
-   async function runCode () {
-     const user = new User(userFormat) //add user
-     const doc = await user.save()
-     console.log(doc)
-     // drop collection await User.deleteMany({})
-   }
-
-   runCode()
-     .catch(error => { console.log(error)})
-
-     res.redirect('/register')
- })
-
- app
-  .post('/activiteiten',  (req,res) => {
-
-    // save Activity with promises
-   const activityFormat = {
-     activity_title: req.body.activity_title,
-     org_name: req.body.org_name,
-     date: req.body.date
-   }
-
-   async function runCode () {
-     const activity = new Activity(activityFormat) //add user
-     const doc = await activity.save()
-     // drop collection await Activity.deleteMany({})
-   }
-
-   runCode()
-     .catch(error => { console.log(error)})
-
-     res.redirect('/activiteiten')
- })
-
 //set up the port for localhost
   app.listen(port, () => {
     console.log(port + " is the way to go")
